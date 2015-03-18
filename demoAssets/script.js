041a3e4b7b0978f1afec575731cb6a9173963753
Dizzypointed <cerngren@gmail.com>
2015-03-18 15:06:51 +0100

    FIX: channelfixes

--- demoAssets/script.js
+++ demoAssets/script.js
@@ -6,54 +6,87 @@ angular.module("app", ["toastCE"])
   function ($scope, toastFactory) {
       var getNextToast = function () {
           var toasts = [{
-              type: "info",
-              title: "Hello! ",
-              message: "Presses will be tracked! <br/><br/><button type='button' class='btn btn-primary' ng-click='press()'>press</button>",
-              timerEnabled: true,
-              scope: {
-                  press: function () {
-                      $scope.pressCount += 1;
-                  }
-              }
-          },
-          {
               type: "success",
-              title: "Hello again! ",
-              message: "This toast will close on timer, but does not show the timer. <br/>These presses will be tracked to! <br/><button type='button' class='btn btn-primary' ng-click='press()'>press</button>",
-              timerEnabled: true,
-              showTimer: false, 
-              scope: {
-                  press: function () {
-                      $scope.pressCount += 1;
-                  }
-              }
-          },
-          {
-              type: "warning",
-              title: "Hello again! ",
-              message: "This warning will not be closed on timer, but can be dissmissed on click!",
+              title: "Welcome! ",
+              message: "This kind of toast is intended to be static, reminding, alerts like \"This item has not been published and is only visible to you\"! This one can be clicked away! ",
+              channel: "tt",
               timerEnabled: false,
               closeOnClick: true,
-              showCloseButton: false
+              showCloseButton: false,
+              showIcon: false
           },
-          {
-              type: "success",
-              title: "Hello again! ",
-              message: "This is what the timer looks like when there's success! ",
-              timerEnabled: true
-          },
-          {
-              type: "warning",
-              title: "Hello again! ",
-              message: "This is what the timer looks like on a warning! ",
-              timerEnabled: true
-          },
-          {
-              type: "danger",
-              title: "Hello again! ",
-              message: "This is what the timer looks like when there's danger! ",
-              timerEnabled: true
-          }];
+              {
+                  type: "info",
+                  title: "Hello! ",
+                  message: "This is custom html and a scope supplied to a toast that will track presses! <br/><br/><button type='button' class='btn btn-primary' ng-click='press()'>press</button>",
+                  timerEnabled: true,
+                  scope: {
+                      press: function () {
+                          $scope.pressCount += 1;
+                      }
+                  }
+              },
+              {
+                  type: "success",
+                  title: "Hello again! ",
+                  message: "This is a toast in a different channel! ",
+                  channel: "bl"
+              },
+              {
+                  type: "danger",
+                  title: "Hello again! ",
+                  message: "This is a toast in a different channel! ",
+                  timerEnabled: true,
+                  channel: "tl"
+              },
+              {
+                  type: "success",
+                  title: "Hello again! ",
+                  message: "This toast will close on timer, but does not show the timer. <br/>These presses will be tracked to! <br/><button type='button' class='btn btn-primary' ng-click='press()'>press</button>",
+                  timerEnabled: true,
+                  showTimer: false,
+                  scope: {
+                      press: function () {
+                          $scope.pressCount += 1;
+                      }
+                  }
+              },
+              {
+                  type: "warning",
+                  title: "Hello again! ",
+                  message: "This warning will not be closed on timer, but can be dissmissed on click!",
+                  timerEnabled: false,
+                  closeOnClick: true,
+                  showCloseButton: false
+              },
+              {
+                  type: "success",
+                  title: "Hello again! ",
+                  message: "This is what the timer looks like when there's success! ",
+                  timerEnabled: true
+              },
+              {
+                  type: "warning",
+                  title: "Hello again! ",
+                  message: "This is what the timer looks like on a warning! ",
+                  timerEnabled: true
+              },
+              {
+                  type: "danger",
+                  title: "Hello again! ",
+                  message: "This is what the timer looks like when there's danger! ",
+                  timerEnabled: true
+              },
+              {
+                  type: "info",
+                  title: "Hello again! ",
+                  message: "This is the end. Go away! ",
+                  channel: "bf",
+                  timerEnabled: false,
+                  closeOnClick: true,
+                  showCloseButton: false,
+                  showIcon: false
+              }];
 
           if (!$scope.index) {
               $scope.index = 0;
